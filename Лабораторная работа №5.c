a30e52847f05c3667702e69a9744f6850fe42c0a
Solidbush <89881313+Solidbush@users.noreply.github.com>
2021-11-06 21:00:05 +0300

    Add Lab5

---
+++ Лабораторная работа №5.c
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+int main(void)
+{
+	int arr[6] = { 34, 45, 56, 67, 78, 89 };
+	for (int i = 0; i < 6; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	int A[2][2] = { { 1, 1 }, { 0, 1 } };
+	int B[2][2] = { { 2, 0 }, { 3, 0 } };
+	int C[2][2] = { {},{} };
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			C[i][j] = 0;
+			for (int k = 0; k < 2; k++)
+			{
+				C[i][j] += A[i][k] * B[k][j];
+			}
+
+		}
+	}
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			printf("%d ", C[i][j]);
+		}
+		printf("\n");
+	}
+}
