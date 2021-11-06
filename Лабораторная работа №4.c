7e24275f43e629aa3d2d27004ae440b314185556
Solidbush <89881313+Solidbush@users.noreply.github.com>
2021-11-06 21:00:00 +0300

    Add Lab4

---
+++ Лабораторная работа №4.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	scanf_s("%d", &a);
+	((a >= 76) && (a <= 78)) ? printf("Yes\n") : printf("No\n");
+	printf("\n");
+	scanf_s("%x", &b);
+	((b & (1 << 6)) > 0)? printf("Yes\n") : printf("No\n");
+}
+
+
