c471d0fb7812020377bc38f05c9f0a1b8001262d
Solidbush <89881313+Solidbush@users.noreply.github.com>
2021-11-06 20:59:54 +0300

    Add Lab3

---
+++ Лабораторная работа №3.c
@@ -0,0 +1,23 @@
+﻿
+
+#include <stdio.h>
+
+int main()
+{
+	int a, b, c;
+	scanf_s("%x", &a);
+	printf("\n");
+	printf("2) %d\n", a);
+	printf("\n");
+	b = a >> 2;
+	printf("3) %x %x\n", a, b);
+	printf("\n");
+	c = ~a;
+	printf("4) %x %x\n", a, c);
+	printf("\n");
+	scanf_s("%x", &b);
+	printf("\n");
+	printf("5) %x", a | b);
+}
+
+
